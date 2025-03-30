01e97b5a1f2671fda807deac70956d3ab4cd9670
Aria Fatah <ariafatah0711@gmail.com>
2025-03-31 05:13:56 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- <Navbar /> -->
-    <Navbar :links="[{ name: 'Notes', path: '/notes/' }, { name: 'Files', path: '/files/' }, { name: 'work', path: '#' }]" />
+    <Navbar :links="[{ name: 'Notes', path: '/notes/' }, { name: 'Files', path: '/files/' }, { name: 'Info', path: '/blog/files' }]" />
     <router-view class="pt-18 sm:pt-25"></router-view>
   </div>
 </template>
